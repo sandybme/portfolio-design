54d4e4250bf3f02dd56413293dbb5a9e741e1655
Praveen <kumarspraveen57@gmail.com>
2024-06-02 02:20:40 +0530

    exposed host name

--- Design 4/next.config.js
+++ Design 4/next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    typescript: {
+      ignoreBuildErrors: true,
+    },
     webpack: (config) => {
       /**
        * Critical: prevents " ⨯ ./node_modules/canvas/build/Release/canvas.node
        * Module parse failed: Unexpected character '�' (1:0)" error
        */
+      
       config.module.rules.push({
         test: /\.node/,
         use: 'raw-loader',
